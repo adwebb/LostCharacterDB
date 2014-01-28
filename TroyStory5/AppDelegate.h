d6cae3c05c5e9b7d42e10cef7420ebf89a93ede2
Andrew Webb <adwebb@gmail.com>
2014-01-28 10:29:55 -0600

    Initial Commit

---
+++ TroyStory5/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TroyStory5
+//
+//  Created by Andrew Webb on 1/28/14.
+//  Copyright (c) 2014 Andrew Webb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
