07b8a4746eae1eec43057fdbd2680a6bde7c2845
Andrew Webb <adwebb@gmail.com>
2014-01-28 22:21:36 -0600

    stretch goals added

---
+++ TroyStory5/AddCharacterViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddCharacterViewController.h
+//  LostCharactersDB
+//
+//  Created by Andrew Webb on 1/28/14.
+//  Copyright (c) 2014 Andrew Webb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddCharacterViewController : UIViewController
+
+@end
---
+++ TroyStory5/AddCharacterViewController.m
@@ -0,0 +1,38 @@
+//
+//  AddCharacterViewController.m
+//  LostCharactersDB
+//
+//  Created by Andrew Webb on 1/28/14.
+//  Copyright (c) 2014 Andrew Webb. All rights reserved.
+//
+
+#import "AddCharacterViewController.h"
+
+@interface AddCharacterViewController ()
+
+@end
+
+@implementation AddCharacterViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
